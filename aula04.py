36d1e57fb3a369d82402dc4c3343f5a61b46fe60
unknown <Bruno>
2024-07-23 19:20:52 -0300

    Aula 04 - Python Curso em Vídeo

---
+++ aula04.py
@@ -0,0 +1,37 @@
+# 1) Exercícios de impressão
+# nome='Bruno'
+# idade=25
+# peso=70.9
+# #Como misturei varios tipos de variaveis, só funcionou com ,
+# # operador + só funciona com tipos de variaveis semelhantes 
+# print(nome,idade,peso)
+
+# #2) Utilize INPUT nos exercicios de impressao 7
+# nome=input('Qaul seu nome: ')
+# idade= input('Qual a sua idade: ')
+# peso=input('Qual o seu peso: ')
+# print('')
+# print(nome+" "+idade+" "+peso)
+
+# Desafio #1
+# 3) Crie um script Python que leia o nome de uma pessoa e mostre 
+# uma mensagem de boas-vindas de acordo com o valor digitado
+# nome=input('Qual o seu nome: ')
+# print('Olá '+nome+" ! Prazer em te conhecer!" )
+
+# Desafio #2
+# Crie um script python que leia o dia, o mês e o ano de nascimento de 
+# uma pessoa e mostre uma mensagem com a data formatada.
+# dia = input("Dia = ")
+# mes = input("Mês = ")
+# ano = input("Ano = ")
+# print("Você nasceu no dia "+dia+" de "+mes+" de "+ano)
+
+# Desafio #3
+# Crie um script Python que leia dois números e tente mostrar a soma 
+# entre eles.
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# soma=n1+n2
+# print("A soma entre os números é:",soma)
+
